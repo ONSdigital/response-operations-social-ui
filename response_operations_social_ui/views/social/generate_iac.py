9df0a7312825314578aaca64687ebcb7f7a7def7
Emilio Ward <emilioward@hotmail.co.uk>
2018-09-27 08:52:19 +0100

    Display format of new HAC

--- response_operations_social_ui/views/social/generate_iac.py
+++ response_operations_social_ui/views/social/generate_iac.py
@@ -13,11 +13,15 @@ logger = wrap_logger(logging.getLogger(__name__))
 @login_required
 def generate_iac():
     case_id = request.form['case_id']
-
     new_iac = case_controller.generate_iac(case_id)
+
     context = build_view_social_case_context(case_id)
     logger.debug("generate_iac", case_id=case_id)
 
-    context['new_iac'] = new_iac
+    def split_string(x, n):
+        return [x[i:i + n] for i in range(0, len(x), n)]
+    split_hac = split_string(new_iac, 4)
+    formatted_hac = (' '.join(map(str, split_hac)))
+    context['new_iac'] = formatted_hac
 
     return render_template('social-view-case-details.html', **context)
